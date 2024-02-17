1e9f08d0f74582675ff383b2d1020614748149ce
Allen Guo <guosonglin001@gmail.com>
2024-02-17 08:12:50 +0000

    add t.sh

---
+++ t.sh
@@ -0,0 +1,63 @@
+docker run -d -it \
+  --name mesa_dev_0 \
+  -v ssh:/root/.ssh \
+  -v repos:/repos \
+  -w /repos \
+  base:latest
+
+################################################################################
+
+# TODO: check
+# .gitlab-ci/container/debian/arm64_build.sh
+
+micromamba install -y meson mako
+micromamba install -y glslang spirv-tools
+micromamba install -y llvm-tools llvmdev
+micromamba install -y clangdev
+micromamba install -y bison
+
+# meson cannot find spirv-tools from conda
+apt install spirv-tools
+apt install libclc-dev
+apt install libdrm-dev
+apt install libx11-dev libx11-xcb-dev
+apt install libxfixes-dev
+apt install -y \
+  libvulkan-dev \
+  libx11-dev \
+  libx11-xcb-dev \
+  libxcb-dri2-0-dev \
+  libxcb-dri3-dev \
+  libxcb-glx0-dev \
+  libxcb-present-dev \
+  libxcb-randr0-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  libxdamage-dev \
+  libxext-dev \
+  libxrandr-dev \
+  libxshmfence-dev \
+  libxtensor-dev \
+  libxxf86vm-dev
+
+# install `llvm-spirv` manully
+# llvm-spirv in conda version dismatch
+mkdir -p demos
+git clone https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git demos
+pushd demos/SPIRV-LLVM-Translator
+git checkout -b v17.0.0 v17.0.0
+cmake -DCMAKE_INSTALL_PREFIX=$PWD/build/install -G Ninja -S$PWD -B$PWD/build
+cmake --build $PWD/build --target install
+popd
+
+################################################################################
+
+# no wayland
+meson . build -Dplatforms=x11 -Dprefix=$PWD/build/install
+
+meson compile -C build
+meson install -C build
+
+################################################################################
+
+meson --help
