de0dc355190175df7863de43f4bb0e6bd4008d7c
Allen Guo <guosonglin001@gmail.com>
2024-12-27 09:33:45 +0000

    wip

--- _demos/t1.build.sh
+++ _demos/t1.build.sh
@@ -4,32 +4,26 @@
 
 ################################################################################
 
-# pushd _demos
-# git clone git@github.com:gglin001/cl_demos.git
-# popd
-# pushd _demos/cl_demos
-# cmake --preset pocl -DWITH_CLINFO=ON -DPOCL_INCLUDE_DIR=/opt/mesa/include -DPOCL_LIB_DIR=/opt/mesa/lib/aarch64-linux-gnu
-# cmake --build $PWD/build --target all
-# ./build/bin/cl_enumopencl
-# ./build/bin/cl_clinfo
+micromamba install xorg-libx11 xorg-libxext xorg-libxfixes
+micromamba install xorg-libxshmfence xorg-libxxf86vm xorg-libxrandr
 
 ################################################################################
 
 PKG_CONFIG_PATH=/opt/spirv-tools/lib/pkgconfig:/opt/llvm-spirv/lib/pkgconfig:/opt/libclc/share/pkgconfig:$PKG_CONFIG_PATH \
   CC=/usr/bin/clang CXX=/usr/bin/clang++ \
   meson setup --reconfigure build \
-  --buildtype=release \
+  --buildtype=debug \
   -Dprefix=$PWD/build/install \
-  -Dplatforms=wayland \
+  -Dplatforms=wayland,x11 \
   -Dopengl=true \
   -Dgles1=disabled \
   -Dgles2=disabled \
-  -Dglx=disabled \
+  -Dglx=dri \
   -Degl=disabled \
   -Dllvm=enabled \
   -Ddraw-use-llvm=true \
   -Dgallium-drivers="llvmpipe" \
-  -Dgallium-opencl=standalone \
+  -Dgallium-opencl=icd \
   -Dgallium-rusticl=false \
   -Dopencl-spirv=true \
   -Dvulkan-drivers="swrast" \
@@ -39,5 +33,18 @@ PKG_CONFIG_PATH=/opt/spirv-tools/lib/pkgconfig:/opt/llvm-spirv/lib/pkgconfig:/op
 
 meson compile -C build
 meson install -C build
+
+################################################################################
+
+git clone git@github.com:gglin001/cl_demos.git
+pushd cl_demos
+cmake --preset sdk
+cmake --build build --target all
+popd
+
+export OCL_ICD_FILENAMES=$PWD/build/install/lib/aarch64-linux-gnu/libMesaOpenCL.so
+
+cl_demos/build/bin/cl_enumopencl
+cl_demos/build/bin/cl_simple
 
 ################################################################################
